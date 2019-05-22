330e416d8f697561ab668b55805e9387652309e6
赵禹光 <769213327@qq.com>
2019-05-22 18:31:06 +0800

    no message

--- server/core/src/main/java/com/ke/schedule/server/core/configuration/CuratorConfiguration.java
+++ server/core/src/main/java/com/ke/schedule/server/core/configuration/CuratorConfiguration.java
@@ -1,11 +1,19 @@
 package com.ke.schedule.server.core.configuration;
 
+import com.ke.schedule.basic.model.ZkAuthInfo;
+import org.apache.curator.framework.AuthInfo;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.RetryNTimes;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * zk配置类
@@ -18,6 +26,9 @@ public class CuratorConfiguration {
 
     @Value("${kob-schedule.zk-connect-string}")
     private String connect;
+    @Value("${kob-schedule.zk-auth-info}")
+    private String zkAuthInfo;
+
 
     @Bean
     public CuratorFramework curator() {
@@ -25,6 +36,9 @@ public class CuratorConfiguration {
                 .connectString(connect)
                 .retryPolicy(new RetryNTimes(Integer.MAX_VALUE, 1000))
                 .connectionTimeoutMs(5000);
+        if(!StringUtils.isEmpty(zkAuthInfo)){
+            builder.authorization("digest", zkAuthInfo.getBytes());
+        }
         CuratorFramework client = builder.build();
         client.start();
         return client;
